8138ea37fbf9f80b4c1c44a0087cbb3d8ec8b9e2
Trung Anh <anhthtservice@gmail.com>
2025-01-07 12:47:42 +0700

    feat: install react query and setup folder structure

---
+++ configs/axios.config.ts
@@ -0,0 +1,35 @@
+import { NEXT_PUBLIC_API_URL } from "@/lib/env";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: NEXT_PUBLIC_API_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log("Request:", config);
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log("Response:", response);
+    return response;
+  },
+  (error) => {
+    console.error("Response error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
